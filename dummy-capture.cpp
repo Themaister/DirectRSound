19d771c81b79c45f316970a13bb62580abb38810
Themaister <maister@archlinux.us>
2011-08-01 01:41:31 +0200

    Add some license headers.

--- dummy-capture.cpp
+++ dummy-capture.cpp
@@ -1,3 +1,19 @@
+/*  DirectRSound - A pure userspace implementation of DirectSound via RSound.
+ *  Copyright (C) 2011 - Hans-Kristian Arntzen
+ * 
+ *  DirectRSound is free software: you can redistribute it and/or modify it under the terms
+ *  of the GNU General Public License as published by the Free Software Found-
+ *  ation, either version 3 of the License, or (at your option) any later version.
+ *
+ *  DirectRSound is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ *  PURPOSE.  See the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along with DirectRSound.
+ *  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
 #include "dummy-capture.hpp"
 #include "log.hpp"
 
